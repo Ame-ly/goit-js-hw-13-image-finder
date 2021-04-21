8dd449afe7c2e7c763ae231a05711e5f7516ee1d
Svitlana Niesmielova <amelia-com@hotmail.com>
2021-04-21 13:32:31 +0300

    changes predeploy

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -2,10 +2,12 @@ const preloaderFactory = selector => {
   return {
     element: document.querySelector(selector),
     hide() {
+      this.element.classList.remove('enable');
       this.element.classList.add('disable');
     },
     show() {
       this.element.classList.remove('disable');
+      this.element.classList.add('enable');
     },
   };
 };
